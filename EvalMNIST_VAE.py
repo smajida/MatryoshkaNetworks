e0cfe4f1769898c83aad9569e191c96001fc7334
Philip Bachman <phil.bachman@gmail.com>
2016-02-01 09:31:23 -0500

    ...

--- EvalMNIST_VAE.py
+++ EvalMNIST_VAE.py
@@ -40,7 +40,7 @@ from MatryoshkaNetworks import InfGenModel, DiscNetworkGAN, GenNetworkGAN
 EXP_DIR = "./mnist"
 
 # setup paths for dumping diagnostic info
-desc = 'test_vae_lrelu_mods_2abc_4bc_no_bn_iwae_50x15'
+desc = 'test_vae_lrelu_mods_2abc_4bc_no_bn_iwae_bias'
 result_dir = "{}/results/{}".format(EXP_DIR, desc)
 inf_gen_param_file = "{}/inf_gen_params.pkl".format(result_dir)
 if not os.path.exists(result_dir):
