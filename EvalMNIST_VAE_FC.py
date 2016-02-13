4562aa70792a476b65489e07589021e66c895619
Philip Bachman <phil.bachman@gmail.com>
2016-02-12 23:27:08 -0500

    current version -- mnist killer

--- EvalMNIST_VAE_FC.py
+++ EvalMNIST_VAE_FC.py
@@ -490,7 +490,7 @@ for epoch in range(5):
             # evaluate costs
             g_result = g_eval_func(imb_img)
             # evaluate costs more thoroughly
-            iwae_bounds = iwae_multi_eval(imb_img, 100,
+            iwae_bounds = iwae_multi_eval(imb_img, 250,
                                           cost_func=iwae_cost_func,
                                           iwae_num=iwae_samples)
             g_result[4] = np.mean(iwae_bounds)  # swap in tighter bound
