6fc7932f9fecd28e1bcd88318fb9a0e2b0fabca3
Philip Bachman <phil.bachman@gmail.com>
2016-02-15 13:39:57 -0500

    current version

--- TestMNIST_VAE.py
+++ TestMNIST_VAE.py
@@ -40,7 +40,7 @@ from MatryoshkaNetworks import InfGenModel, DiscNetworkGAN, GenNetworkGAN
 EXP_DIR = "./mnist"
 
 # setup paths for dumping diagnostic info
-desc = 'test_vae_relu_short_model_basic_kld_no_gates_small_batch'
+desc = 'test_vae_relu_short_model_basic_kld_no_gates_ngf_50'
 result_dir = "{}/results/{}".format(EXP_DIR, desc)
 inf_gen_param_file = "{}/inf_gen_params.pkl".format(result_dir)
 if not os.path.exists(result_dir):
@@ -55,11 +55,11 @@ Xva = Xte
 
 set_seed(1)       # seed for shared rngs
 nc = 1            # # of channels in image
-nbatch = 100      # # of examples in batch
+nbatch = 200      # # of examples in batch
 npx = 28          # # of pixels width/height of images
 nz0 = 64          # # of dim for Z0
 nz1 = 16          # # of dim for Z1
-ngf = 64          # base # of filters for conv layers in generative stuff
+ngf = 50          # base # of filters for conv layers in generative stuff
 ngfc = 256        # # of filters in fully connected layers of generative stuff
 nx = npx*npx*nc   # # of dimensions in X
 niter = 400       # # of iter at starting learning rate
