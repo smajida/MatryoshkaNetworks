1804c0a5f7e182c85dd0bb8bb404308c2d68ca78
Philip Bachman <phil.bachman@gmail.com>
2016-02-20 23:28:00 -0500

    current version

--- TestMNIST_conv.py
+++ TestMNIST_conv.py
@@ -37,13 +37,13 @@ from MatryoshkaNetworks import InfGenModel, DiscNetworkGAN, GenNetworkGAN
 EXP_DIR = "./mnist"
 
 # setup paths for dumping diagnostic info
-desc = 'test_conv_all_noise_000_fix_bin_new_arch'
+desc = 'test_conv_all_noise_000_dyn_bin_new_arch_shallow'
 result_dir = "{}/results/{}".format(EXP_DIR, desc)
 inf_gen_param_file = "{}/inf_gen_params.pkl".format(result_dir)
 if not os.path.exists(result_dir):
     os.makedirs(result_dir)
 
-fixed_binarization = True
+fixed_binarization = False
 # load MNIST dataset, either fixed or dynamic binarization
 data_path = "{}/data/".format(EXP_DIR)
 if fixed_binarization:
@@ -188,29 +188,17 @@ td_module_6 = \
 BasicConvModule(
     filt_shape=(3,3),
     in_chans=(ngf*1),
-    out_chans=(ngf*1),
-    apply_bn=use_bn,
-    stride='single',
-    act_func=act_func,
-    mod_name='td_mod_6'
-) # output is (batch, ngf*1, 28, 28)
-
-# (28, 28) -> (28, 28)
-td_module_7 = \
-BasicConvModule(
-    filt_shape=(3,3),
-    in_chans=(ngf*1),
     out_chans=nc,
     apply_bn=False,
     use_noise=False,
     stride='single',
     act_func='ident',
-    mod_name='td_mod_7'
+    mod_name='td_mod_6'
 ) # output is (batch, c, 28, 28)
 
 # modules must be listed in "evaluation order"
 td_modules = [td_module_1, td_module_2, td_module_3, td_module_4,
-              td_module_5, td_module_6, td_module_7]
+              td_module_5, td_module_6]
 
 ##########################################
 # Setup the bottom-up processing modules #
@@ -289,28 +277,16 @@ BasicConvResModule(
 bu_module_6 = \
 BasicConvModule(
     filt_shape=(3,3),
-    in_chans=(ngf*1),
-    out_chans=(ngf*1),
-    apply_bn=use_bn,
-    stride='single',
-    act_func=act_func,
-    mod_name='bu_mod_6'
-) # output is (batch, ngf*1, 28, 28)
-
-# (28, 28) -> (28, 28)
-bu_module_7 = \
-BasicConvModule(
-    filt_shape=(3,3),
     in_chans=nc,
     out_chans=(ngf*1),
     apply_bn=False,
     stride='single',
     act_func=act_func,
-    mod_name='bu_mod_7'
+    mod_name='bu_mod_6'
 ) # output is (batch, ngf*1, 28, 28)
 
 # modules must be listed in "evaluation order"
-bu_modules = [bu_module_7, bu_module_6, bu_module_5, bu_module_4,
+bu_modules = [bu_module_6, bu_module_5, bu_module_4,
               bu_module_3, bu_module_2, bu_module_1]
 
 #########################################
