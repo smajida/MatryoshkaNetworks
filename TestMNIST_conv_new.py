6e91890a2b642eb263ebf0ff51868807aefe7a90
Philip-Bachman <phil.bachman@gmail.com>
2016-02-25 18:03:30 -0500

    running repeat test on best model, and testing regularized inference

--- TestMNIST_conv_new.py
+++ TestMNIST_conv_new.py
@@ -37,7 +37,7 @@ from MatryoshkaNetworks import InfGenModel, DiscNetworkGAN, GenNetworkGAN
 EXP_DIR = "./mnist"
 
 # setup paths for dumping diagnostic info
-desc = 'test_conv_opt_bu_pert_mods_deeper_gated_nz1_16_narrow_new'
+desc = 'test_conv_opt_bu_pert_mods_deeper_gated_narrow_new'
 result_dir = "{}/results/{}".format(EXP_DIR, desc)
 inf_gen_param_file = "{}/inf_gen_params.pkl".format(result_dir)
 if not os.path.exists(result_dir):
@@ -59,12 +59,12 @@ else:
     Xva = Xte
 
 
-set_seed(123)       # seed for shared rngs
+set_seed(12)      # seed for shared rngs
 nc = 1            # # of channels in image
 nbatch = 200      # # of examples in batch
 npx = 28          # # of pixels width/height of images
 nz0 = 32          # # of dim for Z0
-nz1 = 16          # # of dim for Z1
+nz1 = 8           # # of dim for Z1
 ngf = 32          # base # of filters for conv layers in generative stuff
 ngfc = 128        # # of filters in fully connected layers of generative stuff
 nx = npx*npx*nc   # # of dimensions in X
