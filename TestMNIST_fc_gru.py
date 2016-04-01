4580d9597711be4f89640d124d41bcd0bf01dcc9
Philip-Bachman <phil.bachman@gmail.com>
2016-04-01 10:55:12 -0400

    test deeper fc gru model

--- TestMNIST_fc_gru.py
+++ TestMNIST_fc_gru.py
@@ -82,7 +82,8 @@ gen_depth = 15
 use_ims = True
 inf_mt = 0
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 ntrain = Xtr.shape[0]
 
