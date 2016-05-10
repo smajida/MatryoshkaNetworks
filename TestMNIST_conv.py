cbcb8627c5443ae9226d6cdd67e4e898d912943b
Philip Bachman <phil.bachman@gmail.com>
2016-05-09 22:44:23 -0400

    test mnist hi-res

--- TestMNIST_conv.py
+++ TestMNIST_conv.py
@@ -38,7 +38,7 @@ sys.setrecursionlimit(100000)
 EXP_DIR = "./mnist"
 
 # setup paths for dumping diagnostic info
-desc = 'test_conv_best_6deep'
+desc = 'test_conv_best_2deep_hires'
 result_dir = "{}/results/{}".format(EXP_DIR, desc)
 inf_gen_param_file = "{}/inf_gen_params.pkl".format(result_dir)
 if not os.path.exists(result_dir):
