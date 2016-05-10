717adc84ff6bb81c8a4d78076aa4d2d3c22eb6c6
Philip Bachman <phil.bachman@gmail.com>
2016-05-10 00:33:58 -0400

    import still missing

--- TestMNIST_conv.py
+++ TestMNIST_conv.py
@@ -26,7 +26,7 @@ from load import load_binarized_mnist, load_udm
 #
 # Phil's business
 #
-from ModelBuilders import build_mnist_conv_res
+from ModelBuilders import build_mnist_conv_res, build_mnist_conv_res_hires
 
 sys.setrecursionlimit(100000)
 
