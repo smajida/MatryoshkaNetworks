0dd8d60d3f1704f7955192eb6a5439958b2c3fde
Philip Bachman <phil.bachman@gmail.com>
2016-05-17 00:28:48 -0400

    change test params

--- TestFACES_cond_adver.py
+++ TestFACES_cond_adver.py
@@ -41,7 +41,7 @@ EXP_DIR = "./faces"
 DATA_SIZE = 250000
 
 # setup paths for dumping diagnostic info
-desc = 'test_faces_impute_c010_s005_p010_2xKL'
+desc = 'test_faces_impute_c010_s005_p010_2xKL_2deep'
 result_dir = "{}/results/{}".format(EXP_DIR, desc)
 inf_gen_param_file = "{}/inf_gen_params.pkl".format(result_dir)
 if not os.path.exists(result_dir):
@@ -146,9 +146,9 @@ use_bn = False     # whether to use batch normalization throughout the model
 act_func = 'lrelu'  # activation func to use where they can be selected
 use_td_cond = False
 kld_weight = 2.
-depth_8x8 = 1
-depth_16x16 = 1
-depth_32x32 = 1
+depth_8x8 = 2
+depth_16x16 = 2
+depth_32x32 = 2
 content_weight = 0.04
 style_weight = 0.02
 
